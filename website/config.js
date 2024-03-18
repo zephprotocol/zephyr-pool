02001b8f25645dea952032ccbd34a3f5380cce20
unknown <zephyr_junmxiao@protonmail.com>
2024-03-18 15:02:50 +0800

    init

---
+++ website/config.js
@@ -0,0 +1,22 @@
+var parentCoin = "zephyr";
+
+var api = "http://127.0.0.1:8117";
+var poolHost = "poolhost.com";
+
+// var email = "support@poolhost.com";
+// var telegram = "https://t.me/YourPool";
+var discord = "https://discordapp.com/invite/YourPool";
+var twitter = "https://twitter.com/zephyr_org";
+
+// var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
+
+var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
+var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
+
+var themeCss = "themes/default.css";
+var defaultLang = "en";
+
+// Merged Mining:
+// var api = "http://poolhost/apiMerged";
+// var blockchainExplorer = "http://explorer.ird.cash/?hash={id}#block";
+// var transactionExplorer = "http://explorer.ird.cash/?hash={id}#transaction";
